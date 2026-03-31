0ba4847c58a22edc4e5fa47a670897587a4966e0
VIVEK H S <139493258+smartswagvivek@users.noreply.github.com>
2026-03-31 21:59:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -47,5 +47,9 @@ server.on('error', (error) => {
   console.error('Server failed to start', error);
   process.exit(1);
 });
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 start();
